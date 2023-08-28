8ca4f6aaeb1b0dd70d0bbe6f5da747ba5b1ced73
Pini <pini.tzr@gmail.com>
2023-08-29 00:02:42 +0200

    refactoring

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,18 @@
+import {Link, NavLink} from "react-router-dom";
+
 export const Header = () => {
+
     return (
         <header>
-            <a className="logo" href="/">
-            <img src="/static/media/logo.729fe42e7d23a7906fd5.png"  alt="" />
-            <span>Shopping Cart</span></a>
-            <nav className="navigation"><a aria-current="page" className="link active" href="/">Home</a><a
-                className="link"
-                href="/cart">Cart</a>
+            <NavLink className="logo" to="/"> Shopping Cart</NavLink>
+            <nav className="navigation">
+                <NavLink  className={ ({isActive}) => isActive? "link active" : "link"}   to="/"> Home  </NavLink>
+                <NavLink  className={ ({isActive}) => isActive? "link active" : "link"}  to="/cart"> Cart  </NavLink>
             </nav>
-            <a className="items" href="/cart"><span>Cart: 2</span></a>
+
+
+
+            <NavLink className="items" to="/cart"><span>Cart: 2</span></NavLink>
         </header>
 
 )
